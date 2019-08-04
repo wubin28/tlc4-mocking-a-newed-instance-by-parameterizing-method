880cd47ea1e1a3b664f908dfdb80e759a28e3003
benwu <wubinben@icloud.com>
2019-08-04 10:35:05 +0800

    back to baseline

--- src/main/java/kata/taminglegacycode/tlc4/TestCase.java
+++ src/main/java/kata/taminglegacycode/tlc4/TestCase.java
@@ -2,13 +2,11 @@ package kata.taminglegacycode.tlc4;
 
 public class TestCase {
     public int run() {
-        return run(new TestResult());
-    }
-
-    public int run(TestResult testResult) {
+        TestResult testResult = new TestResult();
 
         // run the tests and populate the test result
 
         return testResult.tests().size();
     }
+
 }
--- src/test/java/kata/taminglegacycode/tlc4/TestCaseTest.java
+++ src/test/java/kata/taminglegacycode/tlc4/TestCaseTest.java
@@ -2,22 +2,8 @@ package kata.taminglegacycode.tlc4;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-
 public class TestCaseTest {
     @Test
     public void should_return_number_of_tests_run() {
-        TestCase testCase = new TestCase();
-        TestResult mockTestResult = mock(TestResult.class);
-        List<kata.taminglegacycode.tlc4.Test> tests = Arrays.asList(new kata.taminglegacycode.tlc4.Test(), new kata.taminglegacycode.tlc4.Test());
-        given(mockTestResult.tests()).willReturn(tests);
-
-        assertThat(testCase.run(mockTestResult), is(2));
     }
 }
