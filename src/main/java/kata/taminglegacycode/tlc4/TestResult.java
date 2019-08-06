35d396f1b47983ac017c51229f1b0984e235ab95
benwu <wubinben@icloud.com>
2019-08-06 16:06:48 +0800

    make TestResult.tests() unsupported

--- src/main/java/kata/taminglegacycode/tlc4/TestResult.java
+++ src/main/java/kata/taminglegacycode/tlc4/TestResult.java
@@ -1,11 +1,9 @@
 package kata.taminglegacycode.tlc4;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TestResult {
     public List<Test> tests() {
-        List<Test> tests = new ArrayList<>();
-        return tests;
+        throw new UnsupportedOperationException("TestResult.tests() not supported yet.");
     }
 }
